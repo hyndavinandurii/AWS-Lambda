68ab7c5e16105bc292b1139c13eb3660c12768b3
ratnakottara <ratnakottara@gmail.com>
2024-06-29 20:22:24 +0530

    Updated index.js file

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello from Dockerized Lambda!');
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+exports.handler = async (event) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Dockerized Lambda!'),
+    };
+    return response;
+};
