f1ced9d8b654b84ee2ef3249ac5a6d1b6fd615c0
ratnakottara <ratnakottara@gmail.com>
2024-06-29 22:37:24 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Dockerized Lambda!'),
+        body: JSON.stringify('Hello from Dockerized Lambda!@'),
     };
     return response;
 };
